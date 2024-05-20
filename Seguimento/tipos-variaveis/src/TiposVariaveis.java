15c22ebea87f1d182cea8bc0dc3011c889c7337b
matheusrv0 <rondonfamiliav@gmail.com>
2024-05-20 20:13:47 -0300

    #

--- Seguimento/tipos-variaveis/src/TiposVariaveis.java
+++ Seguimento/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String nome= "Matheus";
+        int idade = 21;
+        final float VALOR_PI = 3.14f;
+        double NumeroQuebrado = 10.5;
+        long NumeroGrande = 501456548;
     }
-}
+} 
